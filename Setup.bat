63fea74bba4aa632174fcb0756f4dd1c97774cc2
Dimserene <31881806+Dimserene@users.noreply.github.com>
2024-06-30 20:26:05 +0800

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,3 @@
 git clone --recurse-submodules --remote-submodules https://github.com/Dimserene/Dimserenes-Modpack
-xcopy "Dimserenes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /y /e /h /i
+xcopy "Dimserenes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
 pause
