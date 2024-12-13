ea9f4884087786009b932d11f52adecac23e5521
Dimserene <raul0520@google.com>
2024-12-13 23:00:46 +0800

    2024-12-13d

---
+++ Mods/Bmwallet/localization/zh_CN.lua
@@ -0,0 +1,61 @@
+return {
+    descriptions = {
+        Forge = {
+            f_chips={
+                name = '筹码强化',
+                text = {
+                    '{C:chips}+#1#{}筹码'
+                },
+            },
+            f_mult={
+                name = '倍率强化',
+                text = {
+                    '{C:mult}+#1#{}倍率'
+                },
+            },
+            f_x_mult={
+                name = '乘倍率强化',
+                text = {
+                    '{X:red,C:white} X#1# {}倍率'
+                },
+            },
+            f_low_level={
+                name = '低强化等级',
+                text = {
+                    '{C:attention}#1#{}级强化牌'
+                },
+            },
+            f_high_level={
+                name = '高强化等级',
+                text = {
+                    '{C:purple}#1#{}级强化牌'
+                },
+            },
+        },
+    },
+    misc={
+        dictionary = {
+            b_switch = "切换",
+            b_restore = "切牌",
+            b_hide = "藏牌",
+            b_recycle = "换新",
+            b_maintenance = "维修",
+            b_maintenance_2 = "已修复！",
+            b_package = "打包",
+            b_package_2 = "进度：",
+            b_package_3 = "制作完成",
+            b_printing = "印刷",
+            b_printing_2 = "已印刷！",
+            b_forge = "强化",
+            b_forge_practice = "修炼",
+            b_forge_gold = "金色传说！",
+            b_forge_red = "红色品质",
+            b_forge_blue = "蓝色品质",
+            b_forge_lose = "强化失败",
+            b_forge_percentage = "成功率：",
+            b_forge_preach = "修为精进！",
+            b_seal = "冲印",
+            b_seal_2 = "漆火！",
+        },
+    },
+}
